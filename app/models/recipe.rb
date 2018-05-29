9de5eb432649469edbfe5bd593b522c289367d9c
Kyle Chapman <kylechapman09@gmail.com>
2018-05-28 21:35:35 -0500

    added users

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ApplicationRecord
+	belongs_to :user
+
 	has_many :ingredients
 	has_many :directions
+
 	has_attached_file :image, styles: { medium: "400x400#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
